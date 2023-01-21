ab237003784556966bf30e04ff81bb94842b8617
Sam <sammynarduzzi@gmail.com>
2023-01-20 20:36:38 -0800

    transatctions using OOP

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
+}
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+}
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
 }
 
 
@@ -18,12 +37,31 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
+
+
+// const myAccount = new Account("snow-patrol");
+
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// t3 = new Deposit(120.00);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+// console.log('Balance:', balance);
